70380a230ac7900987155382562593a27365c6e1
Ny Antsa Rajaonarison <nyantsa@Nys-MacBook-Pro.local>
2019-10-05 14:58:08 -0700

    Signup form and display error or success message for user signup

--- bookfrontend/src/user/Signup.js
+++ bookfrontend/src/user/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Layout from '../core/Layout'
 import { API } from '../config'
 
@@ -14,7 +15,7 @@ const Signup = () => {
     });
 
     // Destruction
-    const { name, email, password } = values;
+    const { name, email, password, success, error } = values;
 
     // Function returning another function
     const handleChange = name => event =>  {
@@ -24,7 +25,7 @@ const Signup = () => {
     const signup = user  => {
         console.log( name, email, password );
         // Send the data to the backend
-        fetch(`${API}/signup`, {
+        return fetch(`${API}/signup`, {
             method: "POST",
             headers: {
                 Accept: `application/json`,
@@ -32,19 +33,28 @@ const Signup = () => {
             },
             // Convert the object to Json string 
             body: JSON.stringify( user ) 
-        })
+            })
         .then( response => {
             return response.json()
         })
         .catch( err => {
             console.log(err);
         })
+        
     };
 
     const clickSubmit = (event) => {
         // Prevent the page to reload
         event.preventDefault();
+        setValues({ ...values, error: false })
         signup( { name, email, password } )
+        .then(data => {
+            if(data.error) {
+                setValues({...values, error: data.error, success: false })
+            } else {
+                setValues({...values, name:'', email:'', password:'', success: true})
+            }
+        });
     };
 
 
@@ -52,17 +62,27 @@ const Signup = () => {
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input onChange={ handleChange('name') } type="text" className="form-control" />
+                <input 
+                    onChange={ handleChange('name') }
+                    type="text" className="form-control"
+                    value={ name } />
+        
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input onChange={ handleChange('email') } type="email" className="form-control" />
+                <input 
+                    onChange={ handleChange('email') } 
+                    type="email" className="form-control"
+                    value={ email }  />
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input onChange={ handleChange('password') }  type="password" className="form-control" />
+                <input 
+                    onChange={ handleChange('password') }  
+                    type="password" className="form-control"
+                    value={ password }  />
             </div>
 
             <button onClick={clickSubmit} className="btn btn-primary">
@@ -71,14 +91,27 @@ const Signup = () => {
         </form>
     );
 
+    const showError = () => (
+        <div className="alert alert-danger" style={{ display: error ? '' : 'none'}}>
+            { error }
+        </div>
+    );
+
+    const showSuccess = () => (
+        <div className="alert alert-info" style={{ display: success ? '' : 'none'}}>
+            New account is created. Please Signin! <Link to='/signin'>Signin</Link>
+        </div>
+    );
+
     return (
         <Layout 
             className="container col-md-8 offset-md-2" 
             title="Signup" 
             description="Signup to Node React BookStore App"
         >
+            { showSuccess() }
+            { showError() }
             { signupForm() }
-            { JSON.stringify(values) }
         </Layout>
     )
 };
