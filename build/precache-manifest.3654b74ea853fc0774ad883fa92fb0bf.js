be99a9b9783ebc3d9eb3ecece97dc252cb71c829
Ny Antsa Rajaonarison <nyantsa@Nys-MacBook-Pro.local>
2019-10-13 17:20:07 -0700

    Separate ripository for backend and frontend

---
+++ build/precache-manifest.3654b74ea853fc0774ad883fa92fb0bf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690981de4f3253c66ecc83636673f014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fca5155d2a092ef3bd3c",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "891799301ae06dd2659b",
+    "url": "/static/css/main.5d2dc39b.chunk.css"
+  },
+  {
+    "revision": "fca5155d2a092ef3bd3c",
+    "url": "/static/js/2.d12dfad5.chunk.js"
+  },
+  {
+    "revision": "891799301ae06dd2659b",
+    "url": "/static/js/main.a8328177.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
